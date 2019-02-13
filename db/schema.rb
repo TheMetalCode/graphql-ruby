3c44b6cdce329b02cef15a660eb05cb6774abe1e
Jason Hagglund <jhagglund@doximity.com>
2019-02-13 15:42:48 -0800

    Nevermind, UUID isn't that simple

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_13_232343) do
+ActiveRecord::Schema.define(version: 2019_02_13_203126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 2019_02_13_232343) do
     t.string "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.uuid "uuid", default: -> { "gen_random_uuid()" }
     t.index ["user_id"], name: "index_links_on_user_id"
   end
 
