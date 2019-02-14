3f990b898883dd6ae7afe61d74c096efe3cfdc08
Jason Hagglund <jhagglund@doximity.com>
2019-02-13 16:08:38 -0800

    Delete UUID migration

--- db/migrate/20190213232343_add_uuid_to_links.rb
+++
@@ -1,6 +0,0 @@
-class AddUuidToLinks < ActiveRecord::Migration[5.2]
-  def change
-    enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
-    add_column :links, :uuid, :uuid, default: 'gen_random_uuid()' 
-  end
-end
